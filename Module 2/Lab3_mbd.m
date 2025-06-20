90d4016082f55310a63d667e6b32c3aa6786cbef
mennaelsayedd22 <mennaelsayed214@gmail.com>
2025-06-20 16:51:13 +0300

    Add files via upload

---
+++ Module 2/Lab3_mbd.m
@@ -0,0 +1,26 @@
+%% 1. Cell Arrays:
+CellArray={1,5,[1 2 3],'menna',pi}
+disp(CellArray(1))
+disp(CellArray(2))
+disp(CellArray(3))
+disp(CellArray(4))
+disp(CellArray(5))
+
+%% 2. Structures:
+Person.name="menna"
+Person.age=22
+Person.city="cairo"
+disp('my name is ')
+disp(Person.name)
+disp('my age is ')
+disp(Person.age)
+disp('my city is ')
+disp(Person.city)
+%% 3. Character Data:
+
+
+%% 4. Conditional Statements (If):
+
+%% 5. For Loops:
+
+%% 6. Basic Plotting:

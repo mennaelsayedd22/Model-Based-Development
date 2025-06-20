46ed8b95bd39db758bae52742e4d635ebae03fd1
mennaelsayedd22 <mennaelsayed214@gmail.com>
2025-06-20 17:35:14 +0300

    Delete Module 2 directory

--- Module 2/lab5_mbd.m
+++
@@ -1,8 +0,0 @@
-%% Task 1: Creating a Simple Function
-
-%% Task 2: Function Inputs and Outputs
-%% Task 3: Function with Multiple Outputs
-%% Task 4: Nested Functions
-%% Task 5: Function Handles
-%% Task 6: Anonymous Functions
-%% Task 7: Using Built-in Functions
